ed13d3ba2059d0b8be16e1e6350bcc0e69cda44f
uttampun <uttampun50@gmail.com>
2024-09-01 09:11:52 +0545

    home page hero section done

--- src/app/components/HeroMessageBox.tsx
+++ src/app/components/HeroMessageBox.tsx
@@ -4,14 +4,14 @@ import { cn } from "@/utils/cn";
 const HeroMessageBox: React.FC<HeroMessage> = ({
   title,
   description,
-  messageContainer,
+  className,
 }) => {
-  const container = messageContainer.className || 0;
+ 
   return (
     <>
-      <div className={cn({ container })}>
-        <div className="title">{title}</div>
-        <div className="description">{description}</div>
+      <div className={cn(className)}>
+        <div className="title"><h1 className={cn("text-[52px] font-bold leading-normal")}>{title}</h1></div>
+        <div className="description"><p className={cn("text-lg font-normal mt-6")}>{description}</p></div>
       </div>
     </>
   );
